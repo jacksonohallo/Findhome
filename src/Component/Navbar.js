526c3ed5fb592ed3c3d06b7348a295ccd5b1e4dd
jacksonohallo <jacksonohallo@gmail.com>
2022-07-26 14:27:30 +0300

    add styles to components

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,25 +1,54 @@
 import React from 'react'
-import styled from 'styled-components' 
+import styled,{css} from 'styled-components/macro' 
 import { Link } from 'react-router-dom';
+import { menuData } from '../data/MenuData';
 
 
 const Nav = styled.nav`
 height:60px;
-background:red;`
+display:flex;
+justify-content:space-between;
+padding : 1rem 2rem;
+z-index: 100;
+background:#000;`
     ;
 
-const Logo = styled(Link)`
+const NavLink = css`
 color:#fff;
-`   
+display:flex;
+align-items:center;
+padding:0 1rem;
+height:100%;
+cursor:pointer;
+text-decoration:none;
+
+`;
+
+const Logo = styled(Link)`
+${NavLink}
+color:#fff;
+font-style:italic;
+`;  
+const MenuBars = styled.i``;
+const NavMenu = styled.div``;
+const NavMenuLinks = styled(Link)`
+color:#fff;
+`;
 
 const Navbar = () => {
   return (
       <Nav>
-          <Logo>Findhome</Logo>
-          <MenuBar />
-          <NavMenu/>
+          <Logo to="/">Findhome</Logo>
+          <MenuBars/>
+          <NavMenu>
+              {menuData.map((item, index) => (
+                  <NavMenuLinks to={item.link}  key=  {index}>
+                      {item.title}
 
-          <h1>Navbar</h1>
+                  </NavMenuLinks>
+                  
+              )      )}
+          </NavMenu>
     </Nav>
   )
 }
