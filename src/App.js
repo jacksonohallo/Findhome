de21147d4502d1c0f66e8233e41a1a863bdd4e40
jacksonohallo <jacksonohallo@gmail.com>
2022-07-27 15:22:43 +0300

    add page responsiveness

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Hero from "./Component/Hero";
 import InfoSection from "./Component/InfoSection";
 import Navbar from "./Component/Navbar";
+import { InfoData } from "./data/InfoData";
 import { SliderData } from "./data/SliderData";
 import GlobalStyle from "./globalStyle";
 function App() {
@@ -12,7 +13,7 @@ function App() {
     <GlobalStyle/>
       <Navbar />
       <Hero slides={SliderData} />
-      <InfoSection/>
+      <InfoSection{...InfoData}/>
     
     </>
   );
---
+++ src/data/InfoData.js
@@ -0,0 +1,32 @@
+import ImageOne from '../images/image1.jpg'
+import ImageTwo from '../images/image3.jpg'
+
+
+
+
+
+export const InfoData = {
+    heading: ' Explore our beautiful home',
+    paragraphOne:
+        'Lorem Ipsum is simply dummy text of the printing and  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled',
+
+    paragraphTwo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled' ,
+    
+    buttonLabel: 'view Homes',
+    image: ImageOne,
+    reverse: false
+    
+}
+
+export const InfoDataTwo = {
+    heading: ' Explore our  Modern designs',
+    paragraphOne:
+        'Lorem Ipsum is simply dummy text of the printing and  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled',
+
+    paragraphTwo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled' ,
+    
+    buttonLabel: 'view Homes',
+    image: ImageTwo,
+    reverse: false
+    
+}
